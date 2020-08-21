eda1811d61b3a18a07263152ef997eb145a0d3f6
Andre King <andreking1031@gmail.com>
2020-08-21 20:01:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash
   
   return holiday_hash
+  
   # code here
   # remember to return the updated hash
 
